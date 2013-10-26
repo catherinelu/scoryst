cba5ec1c8ed8ce6781a18712f5a3cbfd10218589
Catherine Lu <catherinelu@Catherines-MacBook-Pro.local>
2013-10-26 00:25:15 -0700

    Changed views and settings so template works

--- classallyapp/settings.py
+++ classallyapp/settings.py
@@ -1,3 +1,6 @@
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Django settings for classallyapp project.
 
 DEBUG = True
@@ -61,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -108,6 +111,7 @@ ROOT_URLCONF = 'classallyapp.urls'
 WSGI_APPLICATION = 'classallyapp.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -121,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- classallyapp/urls.py
+++ classallyapp/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+# The next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'classallyapp.views.home', name='home'),
+    url(r'^$', 'classallyapp.views.index', name='index'),
     # url(r'^classallyapp/', include('classallyapp.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # Next line enables the admin:
+    url(r'^admin/', include(admin.site.urls)),
 )
