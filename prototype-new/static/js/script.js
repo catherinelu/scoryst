737f4bb7d3b1aa6fb90bad26c57a8269b915926d
Catherine Lu <catherinelu@Catherines-MacBook-Pro-2.local>
2013-11-23 15:37:51 -0800

    indents -> spaces. working on paperview

--- prototype-new/static/js/script.js
+++ prototype-new/static/js/script.js
@@ -5,11 +5,11 @@ $(function() {
 		var contentHeight = $('.container').outerHeight(true);
 		var viewportHeight = $(window).height() - $('header').height() - $('footer').height();
 		/* If the viewport is larger than the content, the nav fills the viewport. Otherwise,
-     * the nav is at least the size of the content. */
+         * the nav is at least the size of the content. */
 		var navHeight = Math.max(viewportHeight, contentHeight);
 		$('nav').height(navHeight);
 	};
 
-	resizeNav();
-	$(window).resize(resizeNav);
+    resizeNav();
+    $(window).resize(resizeNav);
 });
