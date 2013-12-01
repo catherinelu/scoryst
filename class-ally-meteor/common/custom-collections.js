e5329b266cd53fe6290b488bacf72a4d3ae0e49a
Catherine Lu <catherineglu@gmail.com>
2013-11-30 21:57:16 -0800

    Grade.js uses database

--- class-ally-meteor/common/custom-collections.js
+++ class-ally-meteor/common/custom-collections.js
@@ -4,7 +4,7 @@ Meteor.startup(function() {
 
   // Represents a class that a user is in. Specifies the privileges that the
   // user has for that class.
-  ClassUser = new Meteor.Collection("classuser");
+  ClassUser = new Meteor.Collection("class-user");
 
   // Represents a particular test. Associated with a class.
   Exam = new Meteor.Collection("exam");
@@ -20,4 +20,45 @@ Meteor.startup(function() {
 
   // Represents a student's answer to a question part
   QuestionPartAnswer = new Meteor.Collection("question-part-answer");
+
+  GradedRubric = new Meteor.Collection("graded-rubric");
 });
+
+// // TODO: Remove
+// var examId = Exam.insert({
+//   name: "CS144",
+//   sampleAnswerPath: "/pdf/solutions-cs144.pdf"
+// });
+
+// var questionPartId = QuestionPart.insert({
+//   questionNum: 1,
+//   partNum: 1,
+//   maxScore: 10
+// });
+
+// var rubricId = Rubric.insert({
+//   questionPartId: questionPartId,
+//   points: -5,
+//   description: "Did not give correct explanation"
+// });
+
+// // Rubric that was not chosen
+// Rubric.insert({
+//   questionPartId: questionPartId,
+//   points: 0,
+//   description: "Correct answer"
+// });
+
+// var userExamId = ExamAnswer.insert({ });
+
+// var questionPartAnswerId = QuestionPartAnswer.insert({
+//   examAnswerId: userExamId,
+//   questionPartAnswerId: questionPartId,
+//   graded: true,
+//   graderComments: "Correct answer but incorrect explanation."
+// });
+
+// var gradedRubricId = GradedRubric.insert({
+//   questionAnswerId: questionPartId,
+//   rubricId: rubricId,
+// });
