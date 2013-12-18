12fd568d44e911e35e0b19d4dc622b75544a502d
Catherine Lu <catherineglu@gmail.com>
2013-12-17 19:35:07 -0800

    Rename dropdown.js to script.js and include resizeNav function.

--- classallyproject/static/js/dropdown.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  // show dropdown menu on hover
-  $('.dropdown').hover(function() {
-    $(this).children('.dropdown-menu').show();
-  }, function() {
-    $(this).children('.dropdown-menu').hide();
-  });
-});
---
+++ classallyproject/static/js/script.js
@@ -0,0 +1,22 @@
+$(function() {
+    /* This function calculates and sets the height of the navigation bar. */
+    window.resizeNav = function() {
+        /* The height of the page excluding the header and footer. */
+        var contentHeight = $('.container').outerHeight(true);
+        var viewportHeight = $(window).height() - $('header').height() - $('footer').height();
+        /* If the viewport is larger than the content, the nav fills the viewport. Otherwise,
+         * the nav is at least the size of the content. */
+        var navHeight = Math.max(viewportHeight, contentHeight);
+        $('nav').height(navHeight);
+    };
+
+    resizeNav();
+    $(window).resize(resizeNav);
+
+  // show dropdown menu on hover
+  $('.dropdown').hover(function() {
+    $(this).children('.dropdown-menu').show();
+  }, function() {
+    $(this).children('.dropdown-menu').hide();
+  });
+});
