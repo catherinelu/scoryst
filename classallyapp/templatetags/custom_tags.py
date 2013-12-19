9e6eea26f9a8ae9ec6eff1ef7275c2ab58865667
Karthik Viswanathan <me@karthikv.net>
2013-12-19 11:40:22 -0800

    Fix bug where nav highlights multiple exam pages at once.

--- classallyapp/templatetags/custom_tags.py
+++ classallyapp/templatetags/custom_tags.py
@@ -9,3 +9,13 @@ def path_active(path, pattern):
   if re.search(pattern, path):
     return 'active'
   return ''
+
+@register.simple_tag
+def path_active_course(path, pattern, course):
+  """
+  Returns 'active' if the path matches the given pattern for the provided
+  course, or '' otherwise.
+  """
+  if re.search('course/%d/%s' % (course.pk, pattern), path):
+    return 'active'
+  return ''
