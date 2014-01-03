09f872e2fc3c0560c3af66a367104919f295933d
Karthik Viswanathan <me@karthikv.net>
2014-01-03 15:56:10 -0800

    Add fabfile to simplify deployment.

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.use_ssh_config = True
+env.hosts = ['scoryst']
+
+
+def deploy(reference='origin/master'):
+  """ Deploys the given reference to the scoryst server. """
+  reset = 'git reset --hard %s' % reference
+  apps = ['scorystapp']
+
+  with cd(_get_scoryst_dir()):
+    run('git fetch origin')
+    if confirm('Going to run %s. Proceed?' % reset):
+      run(reset)
+      run('supervisorctl restart scoryst')
+
+    # TODO: reinstate once we add south
+    # if confirm('Migrate all databases?'):
+    #   run('./manage.py syncdb')
+    #   for app in apps:
+    #     run('./manage.py migrate ' + app)
+
+
+def _get_scoryst_dir():
+  """ Returns the directory of the remote scoryst django project. """
+  return '/home/deploy/scoryst'
