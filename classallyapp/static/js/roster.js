5dfa9049a77b4d9b6194ce9fcb51315da6592ff7
Catherine Lu <catherineglu@gmail.com>
2014-01-02 22:39:49 -0800

    Front-end additions to the landing page.

---
+++ classallyapp/static/js/roster.js
@@ -0,0 +1,57 @@
+$(function() {
+  var $confirmDeletionTemplate = $('.confirm-deletion-template');
+  var renderConfirmDeletion = Handlebars.compile($confirmDeletionTemplate.html());
+
+  var $window = $(window);
+  var $deleteButtons = $('.delete');
+
+  $deleteButtons.each(function() {
+    var $delete = $(this);
+    var content = renderConfirmDeletion({ deleteLink: $delete.attr('href') });
+
+    $delete.popover({
+      html: true,
+      content: content,
+      trigger: 'manual',
+      title: 'Are you sure?'
+    });
+  });
+
+  // show popover when user clicks on delete button
+  $deleteButtons.click(function(event) {
+    event.preventDefault();
+    $deleteButtons.popover('hide');
+
+    var $delete = $(this);
+    $delete.popover('show');
+  });
+
+  // hide popover if user clicks outside of it and outside of delete buttons
+  $window.click(function(event) {
+    var $target = $(event.target);
+    var $parents = $target.parents().andSelf();
+
+    if ($parents.filter('.delete').length === 0 &&
+        $parents.filter('.popover').length === 0) {
+      $deleteButtons.popover('hide');
+    }
+  });
+
+  $window.click(function(event) {
+    var $target = $(event.target);
+
+    if ($target.is('.cancel-deletion')) {
+      event.preventDefault();
+      // cancel deletion by closing popovers
+      $deleteButtons.popover('hide');
+    }
+  });
+
+  function resizeRosterList() {
+    var maxHeight = $('.main').height() - $('.roster-scroll').offset().top -
+      parseInt($('.container.roster').css('margin-bottom'), 10);
+    $('.roster-scroll').css({'max-height': maxHeight + 'px'})
+  }
+  resizeRosterList();
+  // $('.main').resize(resizeRosterList);
+});
