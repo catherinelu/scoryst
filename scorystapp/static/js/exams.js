5e24437fa87d670c6cdbee1d7d0ca7be10eb409e
Karthik Viswanathan <me@karthikv.net>
2014-01-02 18:39:46 -0800

    Don't use &nbsp; for spacing. Change add-form name to upload-form.

--- scorystapp/static/js/exams.js
+++ scorystapp/static/js/exams.js
@@ -1,6 +1,6 @@
 $(function() {
-  var infoPopoverText = 'To ensure nothing goes wrong during grading, once students have been mapped' +
-    ' to an exam, exams can no longer be edited or deleted.';
+  var infoPopoverText = 'To ensure nothing goes wrong during grading, once students' +
+    ' have been mapped to an exam, that exam can no longer be edited or deleted.';
 
   var $infoPopover = $('.info-popover');
   $infoPopover.popover({ content: infoPopoverText });
