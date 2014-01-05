0eab738d030d80cd8ecc88c666c79d08edc86577
Karanveer Mohan <karanveer.1992@gmail.com>
2014-01-05 00:24:51 -0800

    fix sorting bug due to introduction of last name

--- scorystapp/static/js/roster.js
+++ scorystapp/static/js/roster.js
@@ -14,14 +14,14 @@ $(function() {
   // Enable sorting
   $('table').tablesorter({
     headers: {  
-      // assign the fourth column (we start counting zero)
-      3: { 
+      // assign the fifth column (we start counting zero)
+      4: { 
         // disable it by setting the property sorter to false
         sorter: false 
       }
     },
     // Sort based on privilege first
-    sortList: [[2,0]]
+    sortList: [[3,0]]
   });
 
   function resizeRosterList() {
