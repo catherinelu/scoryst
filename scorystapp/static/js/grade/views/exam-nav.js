627f6c0aac7484221f89c0adf97a326f2b486668
Karthik Viswanathan <me@karthikv.net>
2014-01-06 20:09:56 -0800

    Show total exam score on grade page.

--- scorystapp/static/js/grade/views/exam-nav.js
+++ scorystapp/static/js/grade/views/exam-nav.js
@@ -27,20 +27,41 @@ var ExamNavView = IdempotentView.extend({
     var activeQuestionPartAnswerId = this.model.get('id');
     var lastQuestionNum = -1;
 
+    // total exam statistics
+    var isExamGraded = true;
+    var examMaxPoints = 0;
+    var examPoints = 0;
+
     questionPartAnswers.forEach(function(questionPartAnswer) {
+      var questionPart = questionPartAnswer.question_part;
+
       // mark question separators
-      if (questionPartAnswer.question_part.question_number !== lastQuestionNum) {
-        lastQuestionNum = questionPartAnswer.question_part.question_number;
-        questionPartAnswer.question_part.starts_new_question = true;
+      if (questionPart.question_number !== lastQuestionNum) {
+        lastQuestionNum = questionPart.question_number;
+        questionPart.starts_new_question = true;
       }
 
       // mark active question part answer
       if (questionPartAnswer.id === activeQuestionPartAnswerId) {
         questionPartAnswer.active = true;
       }
+
+      // compute overall exam statistics
+      // TODO: change this to is_graded once Catherine is done
+      isExamGraded = isExamGraded && questionPartAnswer.graded;
+      examMaxPoints += questionPart.max_points;
+
+      if (questionPartAnswer.graded) {
+        examPoints += questionPartAnswer.points;
+      }
     });
 
-    var templateData = { question_part_answers: questionPartAnswers };
+    var templateData = {
+      is_exam_graded: isExamGraded,
+      exam_max_points: examMaxPoints,
+      exam_points: examPoints,
+      question_part_answers: questionPartAnswers
+    };
     this.$el.html(this.template(templateData));
 
     window.resizeNav();
