8366cdc34ae9fc0c2a613833d501cf67eadc3b2d
Karthik Viswanathan <me@karthikv.net>
2014-01-15 18:13:58 -0800

    Get rid of switch user in celery-worker-start.sh.

--- bin/celery-worker-start.sh
+++ bin/celery-worker-start.sh
@@ -2,7 +2,6 @@
  
 NAME="scoryst-celery"                             # Name of the application
 DJANGODIR=/home/deploy/scoryst                    # Django project directory
-USER=deploy                                       # the user to run as
 DJANGO_SETTINGS_MODULE=scorystproject.settings    # which settings file should Django use
 DJANGO_WSGI_MODULE=scorystproject.wsgi            # WSGI module name
  
@@ -10,8 +9,9 @@ echo "Starting $NAME as `whoami`"
  
 # Activate the virtual environment
 cd $DJANGODIR
+source venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
  
 # Start celery daemon
-exec su -s /bin/bash -c 'source venv/bin/activate && python manage.py celeryd' $USER
+exec python manage.py celeryd
