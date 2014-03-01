33e7ebcda1119f8af5bd6b9e0e6ca4f1aa2d5b88
Karanveer Mohan <karanveer.1992@gmail.com>
2014-03-01 04:07:06 -0800

    Basic mapping question parts code working

---
+++ scorystapp/static/js/map-question-parts.js
@@ -0,0 +1,188 @@
+$(function() { 
+  
+  var imageLoader = new ImageLoader(1, { preloadPage: true, prefetchNumber: 4 }, 
+    { preloadStudent: false});
+  
+  var $optionsTemplate = $('.options-template');
+  var optionsTemplate = Handlebars.compile($optionsTemplate.html());
+  
+  var $questionsSelect = $('.questions');
+  var $partsSelect = $('.parts');
+  var $pagesInput = $('.pages');
+  var $saveButton = $('.save');
+  var $currentPageNum = $('.current-page-number');
+  
+  // Questions is an array with the i-th index corresponding to the number of parts
+  // the (i+1)th question has
+  var questions;
+
+  initTypeAhead();
+  getAllQuestionParts();
+
+  // Initializes the functionality for typeahead.js
+  function initTypeAhead() {
+    // Enables use of handlebars templating engine along with typeahead
+    var T = {};
+    T.compile = function (template) {
+      var compile = Handlebars.compile(template),
+        render = {
+          render: function (ctx) {
+            return compile(ctx);
+          }
+        };
+      return render;
+    };
+
+    // Expected format of each element in array from prefetch:
+    // {
+    //   'name': 'Karanveer Mohan',
+    //   'email': 'kvmohan@stanford.edu',
+    //   'student_id': 01234567,
+    //   'tokens': ['Karanveer', 'Mohan']
+    // }
+    $('.typeahead').typeahead({
+      prefetch: {
+        url: 'get-all-exam-answers/',
+      },
+      template: [
+        '<p><strong>{{name}}</strong></p>',
+        '<p>{{email}} {{studentId}}</p>',
+        '{{#if mapped}}<p class="error">ALREADY MAPPED</p>{{/if}}'
+      ].join(''),
+      limit: 6,
+      engine: T,
+      valueKey: 'name'
+    }).on('typeahead:selected', function (obj, datum) {
+      var url = window.location.href
+      var replaceAfterStr = 'map-question-parts/'
+      var index = url.indexOf(replaceAfterStr);
+      window.location.href = url.substr(0, index + replaceAfterStr.length) + datum['exam_answer_id'];
+    });
+  }
+
+  // Returns an array: [start, start + 1 ... end]
+  function range(start, end) {
+    var foo = [];
+    for (var i = start; i <= end; i++) {
+      foo.push(i);
+    }
+    return foo;
+  }
+
+  function getAllQuestionParts() {
+    $.ajax({
+      url: 'get/',
+      dataType: 'json'
+    }).done(function(data) {
+      questions = data['questions'];
+      // Handlebars can't tolerate one-indexing, so I need to do this shit.
+      $questionsSelect.html(optionsTemplate(range(1, questions.length)));
+      showPart();
+    }).fail(function(request, error) {
+      console.log('Error while getting all question parts');
+    });
+  }
+
+  function showPart() {
+    var questionNumber = $questionsSelect.find(':selected').text();
+    if (questions) {
+      var parts = range(1, questions[questionNumber - 1]);
+      $partsSelect.html(optionsTemplate(parts));
+    }
+    getPagesForQuestionPart();
+  }
+
+  function getPagesForQuestionPart() {
+    var questionNumber = $questionsSelect.find(':selected').text();
+    var partNumber = $partsSelect.find(':selected').text();
+    $.ajax({
+      url: 'get/' + questionNumber + '/' + partNumber + '/',
+    }).done(function(data) {
+      $pagesInput.val(data);
+    }).fail(function(request, error) {
+      console.log('Error while getting pages');
+    });
+  }
+
+  $questionsSelect.change(function() {
+    showPart();
+  });
+
+  $partsSelect.change(function() {
+    getPagesForQuestionPart();
+  });
+
+  $saveButton.click(function() {
+    var questionNumber = $questionsSelect.find(':selected').text();
+    var partNumber = $partsSelect.find(':selected').text();
+    var pages = $pagesInput.val();
+    success = validatePages(pages);
+    
+    if (!success) {
+      $('.error').html('Please enter comma separated pages within the range [1, numPages] in the form: 1,2,3');
+      return;
+    }
+
+    $.ajax({
+      url: 'update/' + questionNumber + '/' + partNumber + '/' + encodeURIComponent(pages) + '/',
+    }).done(function(data) {
+      $('.success').show().delay(3000).fadeOut();
+    }).fail(function(request, error) {
+      alert('Save failed: ' + error);
+    });
+  });
+
+  function validatePages(pages) {
+    pages = pages.split(',');
+    for(var i = 0; i < pages.length; i++) {
+      var page = pages[i];
+      if(isNaN(page)) {
+        return false;
+      }
+      // TODO: Replace num pages with catherine's numpages function in imageloader
+      if (parseInt(page) <= 0 || parseInt(page) > imageLoader.numpages) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Makes the back button work by handling the popState event.
+  $(window).bind('popstate', function() {
+    imageLoader.showPage(imageLoader.curPageNum);
+    $currentPageNum.html(imageLoader.curPageNum);
+  });
+
+  // Implement left and right click. Just changes one page at a time.
+  imageLoader.$previousPage.click(function() {
+    imageLoader.showPageFromCurrent(-1);
+    $currentPageNum.html(imageLoader.curPageNum);
+  });
+
+  imageLoader.$nextPage.click(function() {
+    imageLoader.showPageFromCurrent(+1);
+    $currentPageNum.html(imageLoader.curPageNum);
+  });
+
+
+  $(document).keydown(function(event) {
+    var $target = $(event.target);
+    // If the focus is in an input box or text area, we don't want the page
+    // to be changing
+    if ($target.is('input') || $target.is('textarea')) {
+      return;
+    }
+
+    // Left Arrow Key: Advance the exam
+    if (event.keyCode == 37) {
+       imageLoader.$previousPage.click();
+       return false;
+    }
+
+    // Right Arrow Key: Go back a page in the exam
+    if (event.keyCode == 39) { 
+       imageLoader.$nextPage.click();
+       return false;
+    }
+  });  
+});
---
+++ scorystapp/views/map_question_parts.py
@@ -0,0 +1,157 @@
+from django import shortcuts, http
+from scorystapp import models, decorators
+from scorystapp.views import helpers, grade, grade_or_view
+import json
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def map(request, cur_course_user, exam_id, exam_answer_id=None):
+  """ Renders the map exams page """
+
+  # If no exam_answer_id is given, show the first exam_answer
+  if exam_answer_id is None:
+    exam_answers = models.ExamAnswer.objects.filter(exam_id=exam_id, preview=False)
+    # TODO: How should I handle it best if length is 0?
+    if exam_answers.count():
+      exam_answer_id = exam_answers[0].id
+      return shortcuts.redirect('/course/%s/exams/%s/map/%s/' % (cur_course_user.course.id, exam_id, exam_answer_id))
+
+  return helpers.render(request, 'map-question-parts.epy', {'title': 'Map Exams'})
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def get_all_exam_answers(request, cur_course_user, exam_id):
+  """
+  Returns a json representation of a list where each element has the 
+  exam_id, name, email, student_id of the student (if any student is mapped)
+  along with 'tokens' which is needed by typeahead.js
+  """
+  exam = shortcuts.get_object_or_404(models.Exam, pk=exam_id)
+
+  exam_answers = models.ExamAnswer.objects.filter(exam=exam, preview=False)
+
+  students_to_return = []
+  for exam_answer in exam_answers:
+    course_user = exam_answer.course_user
+    student_to_return = {
+      'exam_answer_id': exam_answer.pk,
+      'name': course_user.user.get_full_name() if course_user else 'unmapped',
+      'email': course_user.user.email if course_user else 'unmapped',
+      'studentId': course_user.user.student_id if course_user else 0,
+      'tokens': [course_user.user.first_name, course_user.user.last_name] if course_user else ['unmapped']
+    }
+    students_to_return.append(student_to_return)
+
+  return http.HttpResponse(json.dumps(students_to_return), mimetype='application/json')
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def get_all_question_parts(request, cur_course_user, exam_id, exam_answer_id):
+  exam = shortcuts.get_object_or_404(models.Exam, pk=exam_id)
+  question_parts = models.QuestionPart.objects.filter(exam=exam).order_by(
+    'question_number', 'part_number')
+  num_questions = question_parts.reverse()[0].question_number if question_parts else 0
+  questions = []
+
+  for i in range(num_questions):
+    questions.append(models.QuestionPart.objects.filter(
+      exam=exam, question_number=i+1).count())
+
+  return http.HttpResponse(json.dumps({'questions': questions}), mimetype='application/json')  
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def get_all_pages_on_question_part(request, cur_course_user, exam_id, exam_answer_id, 
+    question_number, part_number):
+  exam_answer = shortcuts.get_object_or_404(models.ExamAnswer, pk=exam_answer_id)
+  question_part_answer = shortcuts.get_object_or_404(models.QuestionPartAnswer,
+    exam_answer=exam_answer, question_part__question_number=question_number, 
+    question_part__part_number=part_number)
+  return http.HttpResponse(question_part_answer.pages, mimetype='text/html')
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def update_pages_on_question_part(request, cur_course_user, exam_id, exam_answer_id, 
+    question_number, part_number, pages):
+  exam_answer = shortcuts.get_object_or_404(models.ExamAnswer, pk=exam_answer_id)
+  question_part_answer = shortcuts.get_object_or_404(models.QuestionPartAnswer,
+    exam_answer=exam_answer, question_part__question_number=question_number, 
+    question_part__part_number=part_number)
+  if _validate_pages(exam_answer, pages):
+    question_part_answer.pages = pages
+    question_part_answer.save()
+    return http.HttpResponse(status=200)
+  # We validate on the front-end so we just return an error code here.
+  return http.HttpResponse(status=400)
+
+
+def _validate_pages(exam_answer, pages):
+  pages = pages.split(',')
+  for page in pages:
+    if not page.isdigit():
+      return False
+    page = int(page)
+    if page > exam_answer.page_count or page < 1:
+      return False
+  return True
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def get_exam_jpeg(request, cur_course_user, exam_id, exam_answer_id, page_number):
+  """ Gets the jpeg corresponding to exam_answer_id and page_number """
+  return grade_or_view._get_exam_jpeg(request, cur_course_user, exam_answer_id, page_number)
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def get_exam_jpeg_large(request, cur_course_user, exam_id, exam_answer_id, page_number):
+  """ Gets the large jpeg corresponding to exam_answer_id and page_number """
+  return grade_or_view._get_exam_jpeg_large(request, cur_course_user, exam_answer_id, page_number)
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def get_offset_student_jpeg(request, cur_course_user, exam_id, exam_answer_id, offset, page_number):
+  """
+  Gets the jpeg corresponding to page_number for the answer present at offset from the current exam
+  answer. If we reach either bounds (0 or last exam), we return the bound
+  """
+  # Ensure the exam_answer_id exists
+  cur_exam_answer = shortcuts.get_object_or_404(models.ExamAnswer, pk=exam_answer_id)
+  
+  next_exam_answer = grade._get_offset_student_exam(exam_answer_id, offset)
+  return grade_or_view._get_exam_jpeg(request, cur_course_user, next_exam_answer.pk, page_number)
+
+
+@decorators.login_required
+@decorators.valid_course_user_required
+@decorators.course_user_exam_consistent
+@decorators.instructor_or_ta_required
+def get_exam_page_count(request, cur_course_user, exam_id, exam_answer_id):
+  """
+  Returns the number of pages in the exam
+  """
+  exam_answer = shortcuts.get_object_or_404(models.ExamAnswer, pk=exam_answer_id)
+  return http.HttpResponse(exam_answer.page_count)
