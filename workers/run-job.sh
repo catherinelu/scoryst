1850b08fa991e855356263e3e5130c38e401a8f2
Karthik Viswanathan <me@karthikv.net>
2014-03-02 22:19:33 -0800

    Remove remnant files from AWS scripts.

--- workers/run-job.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y python-dev python-setuptools imagemagick
-sudo easy_install pip
-sudo pip install virtualenv
-cd converter
-virtualenv --no-site-packages .
-source bin/activate
-pip install -r requirements.txt
-python converter.py -p payload -d /tmp
