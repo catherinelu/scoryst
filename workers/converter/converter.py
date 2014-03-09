5f8790fb6a2fc5f2c29dcd4e7bddcd5fc6fe2c74
Karthik Viswanathan <me@karthikv.net>
2014-03-09 15:49:44 -0700

    Upload two JPEGs (large and normal) in converter.

--- workers/converter/converter.py
+++ workers/converter/converter.py
@@ -73,6 +73,8 @@ class Converter(worker.Worker):
 
     # convert each page in a separate thread using ImageMagick
     for page_number in range(0, num_pages):
+      local_large_jpeg_path = '%s/%s%d-large.jpeg' % (self.working_dir,
+        local_jpeg_prefix, page_number + 1)
       local_jpeg_path = '%s/%s%d.jpeg' % (self.working_dir, local_jpeg_prefix,
         page_number + 1)
 
@@ -81,7 +83,7 @@ class Converter(worker.Worker):
 
       # multi-threaded:
       threads.append(threading.Thread(target=self._upload_page, args=(local_pdf_path,
-        page_number, local_jpeg_path, bucket, jpeg_prefix)))
+        page_number, local_large_jpeg_path, local_jpeg_path, bucket, jpeg_prefix)))
 
     [thread.start() for thread in threads]
 
@@ -89,32 +91,43 @@ class Converter(worker.Worker):
     [thread.join() for thread in threads]
 
 
-  def _upload_page(self, local_pdf_path, page_number, local_jpeg_path, bucket,
-      jpeg_prefix):
+  def _upload_page(self, local_pdf_path, page_number, local_large_jpeg_path,
+      local_jpeg_path, bucket, jpeg_prefix):
     """
-    Converts a page of a PDF to a JPEG and uploads it to S3.
+    Converts a page of a PDF to two JPEGs (one large and one normal), and
+    uploads both to S3.
 
     Arguments:
     local_pdf_path -- path to local PDF
     page_number -- page number to convert
-    local_jpeg_path -- path to save local JPEG file
+    local_jpeg_path -- path to save local normal JPEG file
+    local_large_jpeg_path -- path to save local large JPEG file
 
     bucket -- the S3 bucket to upload to
-    jpeg_prefix -- file in S3 will be named "[jpeg_prefix][page_number].jpeg"
+    jpeg_prefix -- normal file in S3 will be named "[jpeg_prefix][page_number].jpeg",
+      large file in S3 will be named "[jpeg_prefix][page_number]-large.jpeg"
     """
-    subprocess.check_call(['convert', '-density', '300', '%s[%d]'
-      % (local_pdf_path, page_number), local_jpeg_path])
+    subprocess.check_call(['convert', '-density', '300', '%s[%d]' %
+      (local_pdf_path, page_number), local_large_jpeg_path])
+    subprocess.check_call(['convert', '-resize', '800x800', '%s' %
+      local_large_jpeg_path, local_jpeg_path])
     self._log('Finished converting page %d' % page_number)
 
-    # store converted page in S3
+    # store converted pages in S3
+    large_jpeg_key = s3.Key(bucket)
     jpeg_key = s3.Key(bucket)
 
     # ImageMagick requires 0-indexed page numbers; we use 1-indexed page
     # numbers elsewhere
+    large_jpeg_key.key = '%s%d-large.jpeg' % (jpeg_prefix, page_number + 1)
     jpeg_key.key = '%s%d.jpeg' % (jpeg_prefix, page_number + 1)
+
+    large_jpeg_key.set_contents_from_filename(local_large_jpeg_path)
     jpeg_key.set_contents_from_filename(local_jpeg_path)
 
+    large_jpeg_key.set_acl('public-read')
     jpeg_key.set_acl('public-read')
+
     self._log('Uploaded page %d' % page_number)
 
 
