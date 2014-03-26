057c158e447b6df04566edd38e00e58748b1914f
Karanveer Mohan <karanveer.1992@gmail.com>
2014-03-26 16:05:32 -0700

    make %= escaped by default, %- unescaped for underscore templates

--- scorystapp/static/js/script.js
+++ scorystapp/static/js/script.js
@@ -102,4 +102,14 @@ $(function() {
     // save setting
     $.cookie('navCollapsed', $main.hasClass('collapsed-nav'), { path: '/' });
   });
+
+  // http://stackoverflow.com/questions/22071550/force-backbone-or-underscore-to-always-escape-all-variables
+  if (typeof _ !== 'undefined') {
+    _.templateSettings =
+    {
+      escape: /<%=([\s\S]+?)%>/g,
+      interpolate: /<%-([\s\S]+?)%>/g,
+      evaluate: /<%([\s\S]+?)%>/g
+    };
+  }
 });
