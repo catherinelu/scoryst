873adaa4928a6410a0493d731b4b11e05cde27a8
Karthik Viswanathan <karthik.ksv@gmail.com>
2014-06-22 17:06:47 -0700

    Use SSL and client cert verification for evangelist requests.

--- scorystapp/apis/evangelist.py
+++ scorystapp/apis/evangelist.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 import requests
+import os
 
 def convert_pdf_to_jpegs(s3_pdf_path, s3_jpeg_path, s3_small_jpeg_path,
     s3_large_jpeg_path):
@@ -13,12 +14,22 @@ def convert_pdf_to_jpegs(s3_pdf_path, s3_jpeg_path, s3_small_jpeg_path,
   the JPEG's corresponding page number. Returns the response text from
   Evangelist.
   """
-  response = requests.post(settings.EVANGELIST_URL, data={
+  request_params = {
     's3PDFPath': s3_pdf_path,
     's3JPEGPath': s3_jpeg_path,
     's3SmallJPEGPath': s3_small_jpeg_path,
     's3LargeJPEGPath': s3_large_jpeg_path,
-  })
+  }
+
+  server_cert = os.path.abspath(os.path.dirname(__file__) +
+    '/evangelist-server.crt')
+  client_cert = os.path.abspath(os.path.dirname(__file__) +
+    '/evangelist-client.crt')
+  client_key = os.path.abspath(os.path.dirname(__file__) +
+    '/evangelist-client.key')
+
+  response = requests.post(settings.EVANGELIST_URL, data=request_params,
+    verify=server_cert, cert=(client_cert, client_key))
 
   # raise error if bad status code
   response.raise_for_status()
