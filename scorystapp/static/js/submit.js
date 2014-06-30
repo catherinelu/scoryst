24e68205b9421c0cfb7c79535df053a4832eeae1
Karanveer Mohan <karanveer.1992@gmail.com>
2014-06-30 13:04:40 -0700

    Add pdf instructions for OS X

--- scorystapp/static/js/submit.js
+++ scorystapp/static/js/submit.js
@@ -1,3 +1,19 @@
 $(function() {
-  $('.info-popover').popover();
+  $('.finalized-info-popover').popover();
+
+  var pdfInfoPopoverText = 'Not sure how to create a PDF? ' +
+    'Just follow the instructions given below.';
+
+  var $pdfInfoPopover = $('.pdf-info-popover');
+  $pdfInfoPopover.popover({ content: pdfInfoPopoverText });
+
+  // When the popover is being displayed, highlight the part that gives
+  // instructions on how to create PDFs
+  $($pdfInfoPopover).on('shown.bs.popover', function () {
+    $('.create-pdf-info').addClass('highlighted');
+  });
+
+  $($pdfInfoPopover).on('hidden.bs.popover', function () {
+    $('.create-pdf-info').removeClass('highlighted');
+  });
 });
