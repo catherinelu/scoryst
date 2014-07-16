20d90f3dc3f3a3eba1583a731f8cf01fcd9d0ab1
Karanveer Mohan <karanveer.1992@gmail.com>
2014-07-16 11:22:42 -0700

    Address review comments.

--- bin/restore_specific_backup.py
+++ bin/restore_specific_backup.py
@@ -15,17 +15,13 @@ import time
 import subprocess
 
 
-def main(days_ago):
+def restore_backup(days_ago):
   """
   Restores the last backup that is at least `days_ago` days old.
   """
   # set up django environment
   management.setup_environ(scoryst_settings)
   from django.conf import settings
-
-  # backups happen every 30 minutes; take the backup from the previous hour
-  an_hour_ago = datetime.fromtimestamp(time.time() - 60 * 60)
-  stamp = an_hour_ago.strftime('%m-%d-%Y-%H:30')
 
   connection = s3.S3Connection(settings.AWS_S3_ACCESS_KEY_ID,
     settings.AWS_S3_SECRET_ACCESS_KEY)
@@ -34,8 +30,12 @@ def main(days_ago):
   keys = bucket.list()
   key = get_last_key(keys, days_ago)
 
+  if key == None:
+    print 'No key found to take a backup from!'
+    return
+
   # Fetch backup from S3. Macs can't handle ':' in file names, so we replace them.
-  temp_file_name = '/tmp/backup-%s.dump' % stamp.replace(':', '-')
+  temp_file_name = '/tmp/%s' % key.name
   print 'Fetching file %s to %s' % (key.name, temp_file_name)
   key.get_contents_to_filename(temp_file_name)
 
@@ -45,20 +45,18 @@ def main(days_ago):
 
 
 def get_last_key(keys, days_ago):
-  """
-  Returns the last modified time of the given S3 key. The time is in seconds
-  from the epoch.
-  """
+  """ Loops over the keys and finds the last key that is at least `days_ago` old. """
   last_key = None
   cur_time_in_seconds = time.mktime(time.gmtime())
   min_difference_in_days = float('inf')
+  SECONDS_IN_DAY = 60 * 60 * 24
 
   for key in keys:
     # Get the time of last modification of the key, in seconds
     parsed_datetime = utils.parse_ts(key.last_modified)
     time_in_seconds = time.mktime(parsed_datetime.timetuple())
 
-    difference_in_days = (cur_time_in_seconds - time_in_seconds) / 60.0 / 60.0 / 24
+    difference_in_days = (cur_time_in_seconds - time_in_seconds) / float(SECONDS_IN_DAY)
     # We want the latest backup that is at least `day_ago` old
     if difference_in_days < min_difference_in_days and difference_in_days > days_ago:
       last_key = key
@@ -69,7 +67,7 @@ def get_last_key(keys, days_ago):
 
 if __name__ == '__main__':
   if len(sys.argv) != 2:
-    print 'Usage: ./restore_specific_backup days_ago'
+    print 'Usage: python restore_specific_backup.py days_ago'
     print 'where days_ago is a float that specifies how far back in time we wish to go'
   else:
-    main(float(sys.argv[1]))
+    restore_backup(float(sys.argv[1]))
