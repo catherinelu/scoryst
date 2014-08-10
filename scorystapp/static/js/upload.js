3bef3b92fbb3a685f6ae7290509fc00b3c9b65bc
Catherine Lu <catherineglu@gmail.com>
2014-08-10 15:08:40 -0700

    Use localStorage to stay on the same exam between page loads

--- scorystapp/static/js/upload.js
+++ scorystapp/static/js/upload.js
@@ -1,19 +1,34 @@
 var UploadView = Backbone.View.extend({
+  events: {
+    'change #id_exam_id': 'render',
+  },
+
   REFRESH_DURATION: 3000,
   template: _.template($('.progress-template').html()),
 
   initialize: function(options) {
-    this.$exam = this.$('#id_exam_id');
+    this.$examSelect = this.$('#id_exam_id');
+
+    // Try to get the previous exam value from `localStorage`
+    if (localStorage && localStorage.uploadExamId) {
+      this.$examSelect.val(localStorage.uploadExamId);
+    }
+
     this.$uploadProgress = this.$('.upload-progress');
   },
 
   render: function() {
+    // Update `localStorage.uploadExamId` and set the correct exam value
+    if (localStorage) {
+      localStorage.uploadExamId = this.$examSelect.val();
+    }
+
     var self = this;
 
     // fetch statistics for number of pages uploaded
     $.ajax({
       type: 'GET',
-      url: 'split-pages/' + this.$exam.val() + '/',
+      url: 'split-pages/' + this.$examSelect.val() + '/',
     }).done(function(data) {
       var numTotalPages = data.numTotalPages;
       var numUploadedPages = data.numUploadedPages;
