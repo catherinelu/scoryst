8da20ed59a8eee5e33e3affd1761221d50308a6b
Catherine Lu <catherineglu@gmail.com>
2014-11-03 13:32:49 -0800

    Add new option that allows us to only validate consistency for submissions

--- scorystapp/management/commands/validate_points_graded_consistency.py
+++ scorystapp/management/commands/validate_points_graded_consistency.py
@@ -10,43 +10,49 @@ class Command(BaseCommand):
   option_list = BaseCommand.option_list + (
     make_option('-a', '--assessment_id', help='Specify an assessment to validate'),
     make_option('-s', '--submission_id', help='Specify a submission to validate'),
+    make_option('-o', '--submissions_only', action='store_true', help='Only validate submissions'),
     make_option('-f', '--fix', action='store_true', help='Fix inconsistencies found')
   )
 
   def handle(self, *args, **options):
     assessment_id = options.get('assessment_id', None)
     submission_id = options.get('submission_id', None)
+    submissions_only = options.get('submissions_only', None)
     fix = options.get('fix', None)
 
     if assessment_id:
-      responses = models.Response.objects.filter(submission__assessment=assessment_id)
+      if not submissions_only:
+        responses = models.Response.objects.filter(submission__assessment=assessment_id)
       submissions = models.Submission.objects.filter(assessment=assessment_id)
     elif submission_id:
-      responses = models.Response.objects.filter(submission=submission_id)
+      if not submissions_only:
+        responses = models.Response.objects.filter(submission=submission_id)
       submissions = models.Submission.objects.filter(pk=submission_id)
     else:
-      responses = models.Response.objects.all()
+      if not submissions_only:
+        responses = models.Response.objects.all()
       submissions = models.Submission.objects.all()
 
 
-    for response in responses:
-      if response.points != response._get_points() and response._is_graded():
-        print 'ERROR: response %d is inconsistent in points field' % response.id
-        print response
-        if fix:
-          response.update_response()
-          print 'Fixed'
+    if not submissions_only:
+      for response in responses:
+        if response.points != response._get_points() and response._is_graded():
+          print 'ERROR: response %d is inconsistent in points field' % response.id
+          print response
+          if fix:
+            response.update_response()
+            print 'Fixed'
 
-      if response.graded != response._is_graded():
-        print 'ERROR: response %d is inconsistent in graded field' % response.id
-        print response
-        if fix:
-          response.update_response()
-          print 'Fixed'
+        if response.graded != response._is_graded():
+          print 'ERROR: response %d is inconsistent in graded field' % response.id
+          print response
+          if fix:
+            response.update_response()
+            print 'Fixed'
 
-    print '---------------------------------------------------'
-    print 'DONE VALIDATING RESPONSES!'
-    print '---------------------------------------------------'
+      print '---------------------------------------------------'
+      print 'DONE VALIDATING RESPONSES!'
+      print '---------------------------------------------------'
 
     for submission in submissions:
       points = sum(models.Response.objects.values_list('points', flat=True).filter(
